1b26eb00864d040364bfa614ac00b2515aef4706
XueSeason <xueseason@icloud.com>
2017-10-17 17:42:55 +0800

    add commander

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-// http:\/\/[a-zA-Z.]+\/\d+\/
-const regex = new RegExp('http:\\/\\/[a-zA-Z.]+\\/\\d+\\/')
-console.log(regex.test('http://www.snapvote.me/10/'))
+#!/usr/bin/env node
+
+const path = require('path')
+const program = require('commander')
+const Machine = require('./core/machine')
+const { listCases } = require('./common/util')
+const pkg = require('./package.json')
+
+program
+  .version(pkg.version)
+  .option('-d, --dir [string]', 'test case directory')
+  .parse(process.argv)
+
+let dir
+if (typeof program.dir === 'string') {
+  dir = path.resolve(process.cwd(), program.dir)
+} else {
+  dir = path.resolve(process.cwd(), './')
+}
+
+;(async function () {
+  for (const suit of listCases(dir)) {
+    const machine = new Machine(suit.data)
+    await machine.run()
+  }
+})()
